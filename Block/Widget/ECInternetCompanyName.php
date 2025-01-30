f0bcbc5cfb603294abdf30dff310375e9496c4d9
Randy Esplin <randy.esplin@ecinternet.com>
2025-01-30 12:18:35 -0800

    ECInternetCompanyName - Minor cleanup

--- Block/Widget/ECInternetCompanyName.php
+++ Block/Widget/ECInternetCompanyName.php
@@ -22,19 +22,19 @@ class ECInternetCompanyName extends AbstractWidget
     const ATTRIBUTE_CODE = 'ecinternet_company_name';
 
     /**
-     * @var \ECInternet\CustomerFeatures\Logger\Logger
-     */
-    protected $_logger;
-
-    /**
      * @var \Magento\Customer\Api\CustomerRepositoryInterface
      */
-    private $_customerRepository;
+    private $customerRepository;
 
     /**
      * @var \Magento\Customer\Model\Session
      */
-    private $_customerSession;
+    private $customerSession;
+
+    /**
+     * @var \ECInternet\CustomerFeatures\Logger\Logger
+     */
+    private $logger;
 
     /**
      * ECInternetCompanyName constructor.
@@ -58,14 +58,11 @@ class ECInternetCompanyName extends AbstractWidget
     ) {
         parent::__construct($context, $addressHelper, $customerMetadata, $data);
 
-        $this->_customerRepository = $customerRepository;
-        $this->_customerSession    = $customerSession;
-        $this->_logger             = $logger;
+        $this->customerRepository = $customerRepository;
+        $this->customerSession    = $customerSession;
+        $this->logger             = $logger;
     }
 
-    /**
-     * @inheritdoc
-     */
     public function _construct()
     {
         parent::_construct();
@@ -104,13 +101,13 @@ class ECInternetCompanyName extends AbstractWidget
     public function getECInternetCompanyName()
     {
         try {
-            if ($customer = $this->getCustomer()) {
+            if ($customer = $this->getCustomerById()) {
                 if ($companyName = $customer->getCustomAttribute(self::ATTRIBUTE_CODE)) {
                     return $companyName->getValue();
                 }
             }
         } catch (Exception $e) {
-            $this->log("getECInternetCompanyName() - {$e->getMessage()}");
+            $this->log('getECInternetCompanyName()', ['exception' => $e->getMessage()]);
         }
 
         return '';
@@ -123,20 +120,21 @@ class ECInternetCompanyName extends AbstractWidget
      * @throws \Magento\Framework\Exception\LocalizedException
      * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
-    private function getCustomer()
+    private function getCustomerById()
     {
-        return $this->_customerRepository->getById($this->_customerSession->getCustomerId());
+        return $this->customerRepository->getById($this->customerSession->getCustomerId());
     }
 
     /**
      * Write to extension log
      *
      * @param string $message
+     * @param array  $extra
      *
      * @return void
      */
-    private function log(string $message)
+    private function log(string $message, array $extra = [])
     {
-        $this->_logger->info('Block/Widget/ECInternetCompanyName - ' . $message);
+        $this->logger->info('Block/Widget/ECInternetCompanyName - ' . $message, $extra);
     }
 }
